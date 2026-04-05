d7bbb209055f94834338dd0dad3ac65916523bc5
reham sherif <rehamsherrif@gmail.com>
2026-04-05 05:25:46 +0200

    /

--- ats_website/src/main.tsx
+++ ats_website/src/main.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store} >
-      <BrowserRouter basename={'/ats_website'}>
+      <BrowserRouter basename={'/ATS'}>
         <App />
       </BrowserRouter>
     </Provider>
