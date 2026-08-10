15713446a00d58bb8d1f79fb4c75ddcd0f2698e2
reham sherif <rehamsherrif@gmail.com>
2026-08-10 03:09:09 +0300

    git commit -m "fix database connection string for neon"

--- ats_website/backend/config/connectDB.js
+++ ats_website/backend/config/connectDB.js
@@ -2,11 +2,10 @@ const path = require('path');
 const { Pool } = require('pg');
 require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT||5432),
-     database: process.env.DB_DATABASE,
+   connectionString : process.env.DB_URL,  
+   ssl: {
+    rejectUnauthorized: false // ⚠️ ضروري جداً لاتصال Neon السحابي
+  }
 
 });
 module.exports = pool;
