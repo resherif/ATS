0940acd8e0a2c32dfda3c7d1c527621387330127
reham sherif <rehamsherrif@gmail.com>
2026-08-10 03:38:29 +0300

    some edits regarding to vercel

--- ats_website/frontend/src/main.tsx
+++ ats_website/frontend/src/main.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store} >
-      <BrowserRouter basename={'/ATS'}>
+      <BrowserRouter >
         <App />
       </BrowserRouter>
     </Provider>
